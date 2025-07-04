04a30f535d0cfefe411bd43903becef921a3fbbf
Roland Munyard <rolandmunyard@Julians-MacBook-Pro.local>
2025-07-04 15:11:49 +1000

    Rename favicon to bust Vercel cache

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ return (
     <head>
       {/* Favicon & PWA */}
       <link rel="icon" href="/favicon.ico" />
+      <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="manifest" href="/manifest.json" />
       <meta name="theme-color" content="#FCFAEE" />
 
