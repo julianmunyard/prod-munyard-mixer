11d2d783fd1ee371521d1af42d5e075c240dac22
Julian Munyard <julianmunyard@Julians-MacBook-Pro-2.local>
2025-10-19 14:33:24 +1100

    Remove COOP restriction completely to allow Dropbox popup communication

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
         source: '/:path*',
         headers: [
           { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
-          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin-allow-popups' },
+          { key: 'Cross-Origin-Opener-Policy', value: 'unsafe-none' },
         ],
       },
 
